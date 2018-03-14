4dbd020504be9b411713c2c8d70643dd1fe82873
Aleks Redman <aleksredman@gmail.com>
2018-03-15 00:00:19 +0200

    add socials icons

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Contacts.css';
+import { Icon } from 'antd';
 
 
 
@@ -9,7 +10,11 @@ const Contacts = () => {
       <a href="tel:+380631543556" className={styles.contacts__tel}>
         +38 063 154 35 56
       </a>
-      
+      <div className={styles.contacts__socials_wrap}>
+        <Icon type="facebook" className={styles.contacts__socials}/>
+        <Icon type="google" className={styles.contacts__socials}/>
+        <Icon type="amazon" className={styles.contacts__socials}/>
+      </div>
     </div>
   );
 };
