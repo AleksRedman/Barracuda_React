9d50b60ce06391da55e2ddcccc1e8b127835ef81
Aleks Redman <aleksredman@gmail.com>
2018-03-15 00:03:13 +0200

    included dva menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Menu } from 'antd';
+import { Link } from 'dva/router';
 import styles from './Nav.css';
 
 
@@ -6,7 +8,25 @@ import styles from './Nav.css';
 const Nav = () => {
   return (
     <div>
-      <p>Check</p>
+      <Menu
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        className={styles.nav}
+        theme="dark"
+      >
+        <Menu.Item className={styles.nav__item} key="1">
+          < Link to="/" className={styles.nav__link}>Home</Link>
+        </Menu.Item>
+        <Menu.Item className={styles.nav__item} key="2">
+          < Link to="/Goods" className={styles.nav__link}>Goods</Link>
+        </Menu.Item>
+        <Menu.Item className={styles.nav__item} key="3">
+          < Link to="/Feedback" className={styles.nav__link}>Feedback</Link>
+        </Menu.Item>
+        <Menu.Item className={styles.nav__item} key="4">
+          < Link to="/About_us" className={styles.nav__link}>About us</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
