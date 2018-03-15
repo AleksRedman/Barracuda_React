7a9d677aba6779337dcfa5640e50c3794807a9d9
Aleks Redman <aleksredman@gmail.com>
2018-03-15 13:06:30 +0200

    connected pages Home,Goods,Feedback,About_us

--- src/router.js
+++ src/router.js
@@ -6,16 +6,18 @@ import Contacts from './components/Contacts/Contacts.js';
 import Nav from './components/Nav/Nav.js';
 import SubNav from './components/SubNav/SubNav.js';
 import Copyrights from './components/Copyrights/Copyrights.js';
-import IndexPage from './routes/IndexPage';
+import Home from './routes/Home/Home';
+import Goods from './routes/Goods/Goods.js';
+import Feedback from './routes/Feedback/Feedback.js';
+import About_us from './routes/About_us/About_us.js';
 import styles from './index.css';
 const { Header, Content, Footer, Sider } = Layout;
-
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Layout>
+        <Layout className={styles.wrapper}>
           <Header className={styles.header}>
             <Logo/>
             <Contacts/>
@@ -28,7 +30,10 @@ function RouterConfig({ history }) {
               <SubNav/>
             </Sider>
             <Content>
-              <Route path="/" exact component={IndexPage} />
+              <Route path="/" exact component={Home} />
+              <Route path="/Goods" exact component={Goods} />
+              <Route path="/Feedback" exact component={Feedback} />
+              <Route path="/About_us" exact component={About_us} />
             </Content>
           </Layout>
           <Footer>
