0eec11df404943e862570228585456678d52acf2
Aleks Redman <aleksredman@gmail.com>
2018-03-17 23:40:35 +0200

    add component slider

--- src/components/Big_slider/Big_slider.js
+++ src/components/Big_slider/Big_slider.js
@@ -1,13 +1,67 @@
 import React from 'react';
+import { Carousel } from 'antd';
+import { Button, Icon } from 'antd';
 import styles from './Big_slider.css';
 
 
 
 const Big_slider = () => {
   return (
-    <div>
-      <p>Check</p>
-    </div>
+      <Carousel>
+        <div className={styles.slider__item}>
+          <div className={styles.slider__item_card}>
+            <h2 className={styles.slider__title}>Ajax StreetSiren</h2>
+            <p className={styles.slider__descr}>
+                Працює на відстані до 2000 метрів
+                від Ajax Hubна відкритому просторі
+                або на декількох поверхах бізнес-центру
+            </p>
+            <Button className={styles.slider__button} type="danger" ghost>
+              Подробнее<Icon type="right" />
+            </Button>
+          </div>
+        </div>
+        <div className={styles.slider__item}>
+          <div className={styles.slider__item_card}>
+            <h2 className={styles.slider__title}>Ajax StreetSiren</h2>
+            <p className={styles.slider__descr}>
+                Працює на відстані до 2000 метрів
+                від Ajax Hubна відкритому просторі
+                або на декількох поверхах бізнес-центру
+            </p>
+            <Button className={styles.slider__button} type="danger" ghost>
+              Подробнее<Icon type="right" />
+            </Button>
+          </div>
+        </div>
+        <div className={styles.slider__item}>
+          <div className={styles.slider__item_card}>
+            <h2 className={styles.slider__title}>Ajax StreetSiren</h2>
+            <p className={styles.slider__descr}>
+                Працює на відстані до 2000 метрів
+                від Ajax Hubна відкритому просторі
+                або на декількох поверхах бізнес-центру
+            </p>
+            <Button className={styles.slider__button} type="danger" ghost>
+              Подробнее<Icon type="right" />
+            </Button>
+          </div>
+        </div>
+        <div className={styles.slider__item}>
+          <div className={styles.slider__item_card}>
+            <h2 className={styles.slider__title}>Ajax StreetSiren</h2>
+            <p className={styles.slider__descr}>
+                Працює на відстані до 2000 метрів
+                від Ajax Hubна відкритому просторі
+                або на декількох поверхах бізнес-центру
+            </p>
+            <Button className={styles.slider__button} type="danger" ghost>
+              Подробнее<Icon type="right" />
+            </Button>
+          </div>
+        </div>
+
+      </Carousel>
   );
 };
 
