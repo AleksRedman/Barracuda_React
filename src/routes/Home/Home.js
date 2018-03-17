76d9b87b3db3c17324cf313fc59dab6ad594d8bf
Aleks Redman <aleksredman@gmail.com>
2018-03-17 23:42:09 +0200

    included slider

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { connect } from 'dva';
+import Big_slider from '../../components/Big_slider/Big_slider.js';
 import styles from './Home.css';
 
 function Home() {
   return (
-
-    <div>
-      <p>Home!!!</p>
+    <div className={styles.home}>
+      <Big_slider/>
     </div>
   );
 }
